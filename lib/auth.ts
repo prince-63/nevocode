659aa2a0925378679240fb0ed04e6399cf44827b
Prince Kumar Prasad <prince08833@gmail.com>
2025-03-07 20:47:50 +0530

    i am not using any email and password based authentication in this application and that why i removed all the configuration related to this authentication process

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,4 @@
-import { connectDB } from "@/lib/mongodb";
-import User from "@/models/user";
 import type { NextAuthOptions } from "next-auth";
-import credentials from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 
@@ -15,31 +11,6 @@ export const authOptions: NextAuthOptions = {
     GitHubProvider({
       clientId: process.env.GITHUB_ID || "",
       clientSecret: process.env.GITHUB_SECRET || "",
-    }),
-
-    credentials({
-      name: "Credentials",
-      id: "credentials",
-      credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        await connectDB();
-        const user = await User.findOne({
-          email: credentials?.email,
-        }).select("+password");
-
-        if (!user) throw new Error("Wrong Email");
-
-        const passwordMatch = await bcrypt.compare(
-          credentials!.password,
-          user.password,
-        );
-
-        if (!passwordMatch) throw new Error("Wrong Password");
-        return user;
-      },
     }),
   ],
   session: {
