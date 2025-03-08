2e8028c979a845d6792ec8cd30d65b8eb2b809c9
Prince Kumar Prasad <prince08833@gmail.com>
2025-03-08 17:11:31 +0530

    implemented a prisma and mongodb and also update the authentication machanism

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,19 +1,86 @@
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
+import prisma from "@/lib/prisma";
 
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
     GitHubProvider({
-      clientId: process.env.GITHUB_ID || "",
-      clientSecret: process.env.GITHUB_SECRET || "",
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
   session: {
     strategy: "jwt",
   },
+  callbacks: {
+    async signIn({ user, account }) {
+      // console.log(user, account, profile);
+      try {
+        const existingUser = await prisma.user.findUnique({
+          where: { email: user.email ?? "" },
+        });
+
+        if (existingUser) {
+          const existingProvider = await prisma.account.findFirst({
+            where: {
+              userId: existingUser.id,
+              provider: account?.provider ?? "",
+              providerAccountId: account?.providerAccountId ?? "",
+            },
+          });
+
+          if (!existingProvider) {
+            await prisma.account.create({
+              data: {
+                userId: existingUser.id,
+                provider: account?.provider ?? "unknown",
+                providerAccountId: account?.providerAccountId ?? "unknown",
+                accessToken: account?.access_token ?? null,
+              },
+            });
+            return true;
+          }
+
+          if (existingProvider?.id) {
+            await prisma.account.update({
+              where: { id: existingProvider.id },
+              data: {
+                accessToken:
+                  account?.access_token ?? existingProvider.accessToken,
+              },
+            });
+          }
+          return true;
+        }
+
+        const newUser = await prisma.user.create({
+          data: {
+            email: user.email ?? "",
+            name: user.name ?? "Unknown User",
+            image: user.image ?? null,
+          },
+        });
+
+        await prisma.account.create({
+          data: {
+            userId: newUser.id,
+            provider: account?.provider ?? "unknown",
+            providerAccountId: account?.providerAccountId ?? "unknown",
+            accessToken: account?.access_token ?? null,
+          },
+        });
+
+        return true;
+      } catch (error) {
+        console.error(error);
+        return false;
+      }
+    },
+  },
+  secret: process.env.AUTH_SECRET!,
 };
