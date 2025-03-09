e0129960abf226f79596d535399390c0c8a4efc4
Prince Kumar Prasad <prince08833@gmail.com>
2025-03-09 17:37:01 +0530

    website name changed through csexplore to systemnode

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,8 @@
 import FeatureSection from "@/components/sections/feature-section";
 import HeroSection from "@/components/sections/hero-section";
 import TopicSection from "@/components/sections/topic-section";
-import { cookies } from "next/headers";
 
 export default async function Home() {
-  const cookieStore = await cookies();
-
-  // console.log(cookieStore.getAll());
   return (
     <>
       <HeroSection />
