294191fa5a46290ff1c0742fc72f17c7b2e1b29c
Prince Kumar Prasad <prince08833@gmail.com>
2025-03-13 10:59:13 +0530

    updated master branch

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,17 +1,11 @@
-// import FeatureSection from "@/components/sections/feature-section";
 import GuideSection from "@/components/sections/guide-section";
-// import HeroSection from "@/components/sections/hero-section";
 import IntroSection from "@/components/sections/intro-section";
-// import TopicSection from "@/components/sections/topic-section";
 
 export default async function Home() {
   return (
     <>
       <IntroSection />
-      {/* <HeroSection /> */}
       <GuideSection />
-      {/* <FeatureSection /> */}
-      {/* <TopicSection /> */}
     </>
   );
 }
